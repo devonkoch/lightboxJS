91426a4569a47ca6211f4ae9069a1535175b330d
Devon Koch <devon.r.koch@gmail.com>
2015-10-23 00:08:02 -0700

    added button support, ui

--- app.js
+++ app.js
@@ -6,22 +6,50 @@ function ready(fn) {
   }
 }
 
+var flickrData;
+var photoIndex = 0;
+
+var setPhoto = function(data, photoIndex) {
+  var imgUrl = data.items[photoIndex]['media']['m'].replace("_m", "_b");
+  document.getElementById('title').innerHTML = data.items[photoIndex].title;
+  document.getElementById('thumbnail').src = imgUrl;
+  document.getElementById('popout').src = imgUrl;
+}
+
 ready(function(){
-    
   $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
   
   {
-      tags: "cat",
+      tags: "cats",
       tagmode: "any",
       format: "json"
   },
-  
+
   function(data) {
-      var random = Math.floor(Math.random() * data.items.length);
-      var image_src = data.items[random]['media']['m'].replace("_m", "_b");
-      document.getElementById('title').innerHTML = data.items[random].title;
-      document.getElementById('thumbnail').src = image_src;
-      document.getElementById('popout').src = image_src;
+    setPhoto(data, photoIndex)
+    flickrData = data;
   });
+
+  var prevButton = document.getElementById('previous')
+  var nextButton = document.getElementById('next')
+
+  nextButton.onclick = function() {
+    if(photoIndex >= flickrData.items.length) {
+      alert('this is the last image in the set!')
+    } else {
+      photoIndex += 1;
+      setPhoto(flickrData, photoIndex);
+    }
+  };
+
+  prevButton.onclick = function() {
+    if(photoIndex < 1) {
+      alert('this is the first image in the set!')
+    } else {
+      photoIndex -= 1;
+      setPhoto(flickrData, photoIndex);
+    }
+  };
+
 
 });
