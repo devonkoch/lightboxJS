a68daecad091e425355ae12a06f6e6083c77c1c9
Devon Koch <devon.r.koch@gmail.com>
2015-10-26 18:53:16 -0700

    added 3 headers to php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?
-  php include_once("home.html");
-  header('Access-Control-Allow-Origin: *');
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+include_once("home.html");
 ?>
