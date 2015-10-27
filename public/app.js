f0e15420deb9abeec4a572bfa24b3a5c3daa1f78
Devon Koch <devon.r.koch@gmail.com>
2015-10-26 23:32:58 -0700

    preserve scope w immediately invoking fn; more cleanup

--- public/app.js
+++ public/app.js
@@ -1,54 +1,52 @@
-var letters    = document.querySelectorAll('span');
-var title      = document.getElementById('title');
-var thumbnail  = document.getElementById('thumbnail');
-var popout     = document.getElementById('popout');
-var submit     = document.getElementById('submit');
-var buttons    = document.getElementsByTagName('section')[0];
-var prevButton = document.getElementById('previous');
-var nextButton = document.getElementById('next');
+(function(){
+  var letters    = document.querySelectorAll('span');
+  var title      = document.getElementById('title');
+  var thumbnail  = document.getElementById('thumbnail');
+  var popout     = document.getElementById('popout');
+  var submit     = document.getElementById('submit');
+  var buttons    = document.getElementsByTagName('section')[0];
+  var prevButton = document.getElementById('previous');
+  var nextButton = document.getElementById('next');
 
-var flickrData = null;
-var photoIndex = 0;
+  var flickrData = null;
+  var photoIndex = 0;
 
-function setPhoto(data, photoIndex) {
-  data = data[photoIndex]['@attributes'];
-  thumbnail.src = createSourceUrl(data.farm, data.server, data.id, data.secret);
-  title.innerHTML = data.title !== '' ? data.title + ':' : '(no title)';
-  popout.src = thumbnail.src.replace("_z", "_b"); // changing from medium to large source
-};
-
-domReady(function() {
-
-  submit.onclick = function() {
-
-    buttons.style.display = "block";
-    
-    photoIndex = 0;
-    var input = document.getElementById('username').value;
-    
-    getUserPhotos(input, function(response) {
-      setPhoto(response, photoIndex); // setting first photo
-      flickrData = response;
-    });
-
+  function setPhoto(data, photoIndex) {
+    data = data[photoIndex]['@attributes'];
+    thumbnail.src = createSourceUrl(data.farm, data.server, data.id, data.secret);
+    title.innerHTML = data.title !== '' ? data.title + ':' : '(no title)';
+    popout.src = thumbnail.src.replace("_z", "_b"); // changing from medium to large source
   };
 
-  nextButton.onclick = function() {
-    if(photoIndex >= flickrData.length) {
-      alert('this is the last image in the set!')
-    } else {
-      photoIndex += 1;
-      setPhoto(flickrData, photoIndex);
-    }
-  };
+  domReady(function() {
+    submit.onclick = function() {
 
-  prevButton.onclick = function() {
-    if(photoIndex < 1) {
-      alert('this is the first image in the set!');
-    } else {
-      photoIndex -= 1;
-      setPhoto(flickrData, photoIndex);
-    }
-  };
+      buttons.style.display = "block";
+      var input = document.getElementById('username').value;
+      
+      getUserPhotos(input, function(response) {
+        setPhoto(response, photoIndex); // setting first photo
+        flickrData = response;
+      });
 
-});
+    };
+
+    nextButton.onclick = function() {
+      if(photoIndex >= flickrData.length) {
+        alert('this is the last image in the set!')
+      } else {
+        photoIndex += 1;
+        setPhoto(flickrData, photoIndex);
+      }
+    };
+
+    prevButton.onclick = function() {
+      if(photoIndex < 1) {
+        alert('this is the first image in the set!');
+      } else {
+        photoIndex -= 1;
+        setPhoto(flickrData, photoIndex);
+      }
+    };
+  });
+})();
